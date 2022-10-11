20561cc8c9d8fad3e8908873acf823d02716ac7e
BiancaTuns <nicoleta.tuns@stud.ubbcluj.ro>
2022-10-11 20:02:45 +0300

    Prima pagina modificata

---
+++ Controllers/NewController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tuns_Bianca_Lab1.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Newindex()
+        {
+            return View();
+        }
+
+        public string Indexp(string nume, int varsta)
+        {
+            return "mesaj:"+nume+ "varsta:"+varsta;
+        }
+
+        
+    }
+}
